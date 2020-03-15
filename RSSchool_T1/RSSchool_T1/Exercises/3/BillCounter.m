f456b9141de311316ca1d54e3f0fb77b1a0d3955
Mark <macvik17@mail.ru>
2020-03-15 18:28:43 +0300

    feat: add solution to task3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,18 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    int sum_actual = 0;
+    for (int i = 0; i < bill.count; i++) {
+        if (i != index) {
+            sum_actual += [bill[i] intValue];
+        }
+    }
+    sum_actual /= 2;
+    if (sum_actual == [sum intValue])
+        return @"Bon Appetit";
+    else {
+        return [[NSNumber numberWithInt: ([sum intValue] - sum_actual)] stringValue];
+    }
 }
 
 @end
